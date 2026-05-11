58e6954911d8e9a2def3163bcb9a9de553e5f8cf
Alina <churaeva2007@gmail.com>
2026-05-11 20:16:49 +0300

    Добавление asm7.cpp

---
+++ asm7.cpp
@@ -0,0 +1,55 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int a, b, c, d, e, y;
+
+    char prn[] = "Введите a, b, c, d, e:\n";
+    char scn[] = "%d %d %d %d %d";
+    char out[] = "y = %d\n";
+
+    __asm {
+        lea eax, prn
+        push eax
+        call printf
+        add esp, 4
+
+        lea eax, e
+        push eax
+        lea eax, d
+        push eax
+        lea eax, c
+        push eax
+        lea eax, b
+        push eax
+        lea eax, a
+        push eax
+        lea eax, scn
+        push eax
+        call scanf
+        add esp, 24  
+
+        mov eax, a       
+        add eax, b       
+        cdq              
+        idiv c           
+
+        mov ecx, eax     
+
+        mov eax, d       
+        imul eax, e      
+
+        sub ecx, eax     
+        mov y, ecx       
+
+        push y
+        lea eax, out
+        push eax
+        call printf
+        add esp, 8      
+    }
+
+    return 0;
+}
